85aad8b4408cebca135b2e15c2446e7399f23ac4
Mapachaal <73990397@continental.edu.pe>
2026-05-29 16:21:26 -0500

    Add new tasks to tareas.js

--- tareas.js
+++ tareas.js
@@ -1,4 +1,6 @@
-console.log("Jugar Projecct Zomboid");
 console.log("Terminar tarea de BD2");
-console.log("Terminar de ver la película Hail Mary project");
+console.log("Jugar Projecct Zomboid");
 console.log("Comprar comida para las mamis gatunas");
+console.log("Terminar de ver la película Hail Mary project");
+console.log("Comprar el libro de Hail Mary project");
+console.log("Generar una nueva playlist espacial y aprender nuevos datos de animales");
