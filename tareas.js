7b41a12f07e1948496d677463aae6a2274f0137e
Mapachal ^^ <73990397@continental.edu.pe>
2026-05-29 16:04:24 -0500

    segundo commit

--- tareas.js
+++ tareas.js
@@ -1,1 +1,4 @@
-console.log("Lista de Tareas");
+console.log("Jugar Projecct Zomboid");
+console.log("Terminar tarea de BD2");
+console.log("Terminar de ver la película Hail Mary project");
+console.log("Comprar comida para las mamis gatunas");
